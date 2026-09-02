3dab05990ea3a2a2b4e5721d1b967146b42b0dc8
LORENZZO GABRIEL ALMEIDA DOS SANTOS <lorenzzo_santos@estudante.sesisenai.org.br>
2026-09-01 21:52:02 -0300

    Capa terminada. Primeira tela foi iniciada mas ocorreu erro ao tentar mudar a centralização das letras

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnlogin").addEventListener("click", function() {
+    document.getElementById("tela-capa").style.display = "none";
+    document.getElementById("tela-inicial").style.display = "flex";
+})
