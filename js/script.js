a7250f449e2970a007de549fda574b14e7c86ea6
LORENZZO GABRIEL ALMEIDA DOS SANTOS <lorenzzo_santos@estudante.sesisenai.org.br>
2026-09-03 21:16:28 -0300

    Capa Finalizada e primeiro level quase pronto. Botões ainda não foram feitos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-document.getElementById("btnlogin").addEventListener("click", function() {
+document.getElementById("btnentrar").addEventListener("click", function() {
     document.getElementById("tela-capa").style.display = "none";
     document.getElementById("tela-inicial").style.display = "flex";
+    document.getElementById("pe").style.display = "flex"
 })
