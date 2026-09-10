3f5aedbe678d7dcbc14f89e93c4c8514c592a499
LORENZZO GABRIEL ALMEIDA DOS SANTOS <lorenzzo_santos@estudante.sesisenai.org.br>
2026-09-09 21:43:02 -0300

    Só falta mudar os textos

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ document.getElementById("btnentrar").addEventListener("click", function() {
     document.getElementById("tela-inicial").style.display = "flex";
     document.getElementById("pe").style.display = "flex"
 })
+
+document.getElementById("sair").addEventListener("click", function() {
+    document.getElementById("tela-capa").style.display = "flex";
+    document.getElementById("tela-inicial").style.display = "none";
+    document.getElementById("pe").style.display = "none"
+})
 function mudarSkin(nomeDaSkin) {
     const container = document.getElementById("body");
 
     container.className = "container";
 
     container.classList.add(nomeDaSkin);
+}
+function voltar() {
+
 }
