e488a9be88877765e3c3ee49ac0caae2367ce906
LORENZZO GABRIEL ALMEIDA DOS SANTOS <lorenzzo_santos@estudante.sesisenai.org.br>
2026-09-14 21:23:49 -0300

    Terminei

--- js/script.js
+++ js/script.js
@@ -19,3 +19,68 @@ function mudarSkin(nomeDaSkin) {
 function voltar() {
 
 }
+document.getElementById("btn1").addEventListener("click", function() {
+    document.getElementById("Texto1Lvl0").style.display = "flex"
+    document.getElementById("TextoPool").style.display = "none"
+    document.getElementById("TextoRun").style.display = "none"
+    document.getElementById("TextoFun").style.display = "none"
+    document.getElementById("TextoGray").style.display = "none"
+
+    document.getElementById("TituloLevel0").style.display = "flex"
+    document.getElementById("TituloPool").style.display = "none"
+    document.getElementById("TituloRun").style.display = "none"
+    document.getElementById("TituloFun").style.display = "none"
+    document.getElementById("TituloGray").style.display = "none"
+})
+document.getElementById("btn2").addEventListener("click", function() {
+    document.getElementById("Texto1Lvl0").style.display = "none"
+    document.getElementById("TextoPool").style.display = "flex"
+    document.getElementById("TextoRun").style.display = "none"
+    document.getElementById("TextoFun").style.display = "none"
+    document.getElementById("TextoGray").style.display = "none"
+
+    document.getElementById("TituloLevel0").style.display = "none"
+    document.getElementById("TituloPool").style.display = "flex"
+    document.getElementById("TituloRun").style.display = "none"
+    document.getElementById("TituloFun").style.display = "none"
+    document.getElementById("TituloGray").style.display = "none"
+})
+document.getElementById("btn3").addEventListener("click", function() {
+    document.getElementById("Texto1Lvl0").style.display = "none"
+    document.getElementById("TextoPool").style.display = "none"
+    document.getElementById("TextoRun").style.display = "flex"
+    document.getElementById("TextoFun").style.display = "none"
+    document.getElementById("TextoGray").style.display = "none"
+
+    document.getElementById("TituloLevel0").style.display = "none"
+    document.getElementById("TituloPool").style.display = "none"
+    document.getElementById("TituloRun").style.display = "flex"
+    document.getElementById("TituloFun").style.display = "none"
+    document.getElementById("TituloGray").style.display = "none"
+})
+document.getElementById("btn4").addEventListener("click", function() {
+    document.getElementById("Texto1Lvl0").style.display = "none"
+    document.getElementById("TextoPool").style.display = "none"
+    document.getElementById("TextoRun").style.display = "none"
+    document.getElementById("TextoFun").style.display = "flex"
+    document.getElementById("TextoGray").style.display = "none"
+
+    document.getElementById("TituloLevel0").style.display = "none"
+    document.getElementById("TituloPool").style.display = "none"
+    document.getElementById("TituloRun").style.display = "none"
+    document.getElementById("TituloFun").style.display = "flex"
+    document.getElementById("TituloGray").style.display = "none"
+})
+document.getElementById("btn5").addEventListener("click", function() {
+    document.getElementById("Texto1Lvl0").style.display = "none"
+    document.getElementById("TextoPool").style.display = "none"
+    document.getElementById("TextoRun").style.display = "none"
+    document.getElementById("TextoFun").style.display = "none"
+    document.getElementById("TextoGray").style.display = "flex"
+
+    document.getElementById("TituloLevel0").style.display = "none"
+    document.getElementById("TituloPool").style.display = "none"
+    document.getElementById("TituloRun").style.display = "none"
+    document.getElementById("TituloFun").style.display = "none"
+    document.getElementById("TituloGray").style.display = "flex"
+})
